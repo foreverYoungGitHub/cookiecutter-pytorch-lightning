58b62e6c07c62596402381f6ede3e89dff12c564
Yang Liu <yang.liu@shopify.com>
2025-06-18 21:55:03 -0400

    change the --torch-backend auto back, will re enable it once it is official release from uv

--- {{cookiecutter.project_name}}/.devcontainer/postCreateCommand.sh
+++ {{cookiecutter.project_name}}/.devcontainer/postCreateCommand.sh
@@ -4,7 +4,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Install Dependencies
-uv sync --torch-backend=auto
+uv sync
 
 # Install pre-commit hooks
 uv run pre-commit install --install-hooks
